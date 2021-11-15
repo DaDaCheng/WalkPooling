d81618bf0d511a60928630e6aaa3fe03eb48af28
Cheng <cheng.shi@unibas.ch>
2021-11-15 09:42:30 +0100

    gic init

---
+++ software/GIC/layers/__init__.py
@@ -0,0 +1,4 @@
+from .gcn import GCN
+from .readout import AvgReadout
+from .discriminator import Discriminator, Discriminator_cluster
+from .cluster import Clusterator
---
+++ software/GIC/models/__init__.py
@@ -0,0 +1,2 @@
+from .gic import GIC
+from .logreg import LogReg
